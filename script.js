6377d067bbc0a68966267f00ddd217be3add4fe5
cami-js <camirivenson@gmail.com>
2024-04-18 16:11:44 -0300

    Implementacion de grafico de perdida

--- script.js
+++ script.js
@@ -15,7 +15,10 @@ document.addEventListener('DOMContentLoaded', function() {
         botonEntrenar.disabled = true; 
         const xs = tf.tensor2d([[-6], [-5], [-4], [-3], [-2], [-1], [0], [1], [2]]);
         const ys = tf.tensor2d([[-6], [-4], [-2], [0], [2], [4], [6], [8], [10]]); // Valores de y correspondientes a la función y = 2x + 6
-        await model.fit(xs, ys, {epochs: 1000});
+        const history = await model.fit(xs, ys, {epochs: 1000, callbacks: tfvis.show.fitCallbacks(
+            {name: 'Entrenamiento'}, 
+            ['loss']            
+        )});
         entrenamientoCompletado = true;
         const mensajeElement = document.getElementById('mensajeEntrenamiento');
         mensajeElement.innerText = 'Entrenamiento completado.';
